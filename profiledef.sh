ae799d9e671ae2575df2aafa8ebb2c2638391fd6
demonkillerr <gaurangvishwakarma619@gmail.com>
2024-02-03 00:43:29 +0530

    update iso-name

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="alg-gnome"
+iso_name="alg-gnome-pure"
 iso_label="ALG_GNOME_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="DemonKiller <https://demonkiller.tech>"
 iso_application="ALG Live/Rescue CD"
